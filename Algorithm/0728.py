54638cd0be98352b43a5394bc0a69d92cf1d7332
jkim1209 <jwk143@gmail.com>
2025-09-15 09:30:06 +0900

    feat: Add solutions for 0728.py

---
+++ Algorithm/0728.py
@@ -0,0 +1,22 @@
+#%%
+# Problem 1. Lv.0 두 수의 합 구하기
+# https://school.programmers.co.kr/learn/courses/30/lessons/120802
+# Solution
+def solution(num1, num2):
+    answer = int(num1) + int(num2)
+    return answer
+
+
+#%%
+# Problem 2. 알고리즘 수업 - 알고리즘의 수행 시간 1
+# https://www.acmicpc.net/problem/24262
+# Solution
+print(1)
+print(0)
+
+
+#%%
+# Problem 4. Lv.4 호텔 방 배정
+# https://school.programmers.co.kr/learn/courses/30/lessons/64063
+# Solution
+
